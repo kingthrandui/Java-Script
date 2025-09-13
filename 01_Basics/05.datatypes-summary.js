12dcbf95e3345f1e1c0ceae70acc8e8032ceb6d3
Naman <pratapsinghnaman392@gmail.com>
2025-09-13 12:12:34 +0000

    Data Types Summary

---
+++ 01_Basics/05.datatypes-summary.js
@@ -0,0 +1,22 @@
+/*                                                  DATATYPES SUMMARY
+================================================================================
+1. Primitive Datatypes:
+   - Number: Represents both integer and floating-point numbers.
+   - String: Represents a sequence of characters.
+   - Boolean: Represents a logical entity and can have two values: true or false.
+   - Null: Represents the intentional absence of any object value.
+   - Undefined: Indicates that a variable has not been assigned a value.
+   - Symbol: Represents a unique identifier, often used as object property keys.
+   - BigInt: Represents integers with arbitrary precision, allowing for the representation of very large numbers.
+
+2. Non-Primitive Datatypes 0r Reference Datatypes:
+   - Object: A collection of properties, where each property is defined as a key-value pair.
+   - Array: A special type of object used to store ordered collections of values.
+   - Function: A callable object that encapsulates a block of code to be executed.
+
+
+
+
+
+
+*/ 
