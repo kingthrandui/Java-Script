2b5739f45ef995c93c1c3e1136e0cfb512531b7b
Naman <pratapsinghnaman392@gmail.com>
2025-09-13 12:13:14 +0000

    Comparison Operators

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1); //true
+// console.log(2<1); //false
+// console.log(2>=1); //true
+// console.log(2<=1); //false
+// console.log(2==1); //false
+// console.log(2!=1); //true
+
+// console.log("2">1)
+
+console.log(null>0); //false
+console.log(null==0);
+console.log(null>=0); //true
+
+//strict check=== it checks value as well as data type
+console.log(1=="1"); //true
+console.log(1==="1"); //false
